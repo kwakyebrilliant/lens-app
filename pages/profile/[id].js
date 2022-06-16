ba4320346476e8939623f08d0740d50b6112d235
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-06-16 20:26:50 +0000

    id some commits

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,8 +1,10 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from 'react';
 import { client, getProfiles } from "../../api";
+import Image from 'next/image';
 
 export default function Profile() {
+    const [profile, setProfile] = useState()
     const router = useRouter()
     const { id } = router.query
     useEffect(() => {
@@ -15,14 +17,32 @@ export default function Profile() {
         try {
             const response = await client.query(getProfiles, { id }).toPromise()
             console.log('response: ', response)
+            setProfile(response.data.profiles.items[0])
         } catch (err) {
             
         }
     }
 
+    if (!profile) return null
+
     return (
         <div>
-            {id}
+            {
+                profile.picture ? (
+                    <Image 
+                        width="200px"
+                        height="200px"
+                        src={profile.picture.original.url}
+                    />
+                ) : (
+                    <div style={{ width: '200px', height: '200px', backgroundColor: 'black'}} />)
+            }
+            <div>
+                <h4>{profile.handle}</h4>
+                <p>{profile.bio}</p>
+                <p>Followers: {profile.stats.totalFollowers}</p>
+                <p>Following: {profile.stats.totalFollowing}</p>
+            </div>
         </div>
     )
 }

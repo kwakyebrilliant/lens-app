9a423350356443b859a8718ca301a2005efbf1d6
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-06-16 19:43:33 +0000

    id commits

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,8 +1,24 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from 'react';
+import { client, getProfiles } from "../../api";
 
 export default function Profile() {
     const router = useRouter()
     const { id } = router.query
+    useEffect(() => {
+        if (id) {
+            fetchProfile()
+        }
+}, [id])
+
+    async function fetchProfile() {
+        try {
+            const response = await client.query(getProfiles, { id }).toPromise()
+            console.log('response: ', response)
+        } catch (err) {
+            
+        }
+    }
 
     return (
         <div>
